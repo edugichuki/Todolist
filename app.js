608245aabbc8d171d09bee8c7138a22f9f95a4f6
edugichuki <muriithigichuki854@gmail.com>
2023-07-06 08:23:18 +0300

    Edit login credentials

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const password = process.env.MONGO_PASS;
 
 const myDatabase = process.env.MONGO_DATABASE;
 
-const url = "mongodb+srv://muriithigichuki854:bmx2020quantum@cluster0.1lwat6t.mongodb.net/todolistDB?retryWrites=true&w=majority"
+const url = `mongodb+srv://${userName}:${password}@cluster0.1lwat6t.mongodb.net/todolistDB?retryWrites=true&w=majority`
 
 mongoose
   .connect(
